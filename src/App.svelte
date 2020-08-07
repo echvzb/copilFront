20eb9908f153a7cba9b3aeb0748b7a4ec3c81aa2
Eduardo Chavez Barreto <eduardochvzb@localhost.localdomain>
2020-08-07 17:05:32 -0500

    navbar

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Main from './components/Main.svelte'
+</script>
+<Main></Main>
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap');
+
+	:global(html){
+		font-family: 'Poppins', sans-serif;
+	}
+</style>
+
